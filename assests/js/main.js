35b7a1af71144e0076ad77a1988ce36c59a64478
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-08 14:37:56 +0200

    add getElementById for all element

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,7 +1,16 @@
-// seleziono la lampadina spenda tramite id
+// seleziono la lampadina accesa tramite id
 const on_lamp_el = document.getElementById("on-lamp");
+
+// seleziono la lampadina spenta tramite id
+const off_lamp_el = document.getElementById("on-lamp");
+
+// seleziono il bottone tramite id
+const switch_el = document.getElementById("switch");
+
 // stampo in console per controllare se è corretto
-console.log(on_lamp_el);
+console.log(on_lamp_el, off_lamp_el, switch_el);
 
 // tramite la proprietà style posso cambiare il display e non farla vedere in pagina
 on_lamp_el.style.display = "none";
+
+
