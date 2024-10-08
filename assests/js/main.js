c733dbe13597c2026b45b6a9d0730afcd320bf6b
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-08 14:35:00 +0200

    edit display of the on lamp

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+// seleziono la lampadina spenda tramite id
+const on_lamp_el = document.getElementById("on-lamp");
+// stampo in console per controllare se è corretto
+console.log(on_lamp_el);
+
+// tramite la proprietà style posso cambiare il display e non farla vedere in pagina
+on_lamp_el.style.display = "none";
